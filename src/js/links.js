4a90113770373fe8d49026ccd70257133cdb67ec
GuilhermedMS <155796519+GuilhermedMS@users.noreply.github.com>
2024-01-26 16:31:58 -0300

    limit

---
+++ src/js/links.js
@@ -0,0 +1,12 @@
+// SOCIALS
+
+const steam = () => (window.open("https://steamcommunity.com/id/guilhermel3"));
+const instagram = () => (window.open("https://www.instagram.com/gui.dmrss/"));
+const github = () => (window.open("https://github.com/GuilhermedMS"));
+const spotify = () => (window.open("https://open.spotify.com/user/31dfhwnm4lnz2wc6lqjbwsuujqim?si=9ff0ec132fbd414c"));
+const pinterest = () => (window.open("https://br.pinterest.com/guimtw/   /"));
+
+// outros
+
+const arch = () => (window.open("https://discord.gg/6Nq6z7XtU8"));
+const biel = () => (window.open("https://discord.gg/6Nq6z7XtU8"));
